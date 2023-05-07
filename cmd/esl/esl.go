74e13de2e6330afcae5d2af7666176dfb7a811b0
Thomas Stromberg <t+github@stromberg.org>
2023-05-07 12:15:57 -0400

    Show correct pids

--- cmd/esl/esl.go
+++ cmd/esl/esl.go
@@ -19,9 +19,9 @@ func main() {
 	handler := func(r *eslogger.Row) bool {
 		switch o.Kind {
 		case "open":
-			fmt.Printf("%s [%d] opened %s\n", r.Process.Executable.Path, r.Process.ParentAuditToken.Pid, r.Event.Open.File.Path)
+			fmt.Printf("%s [%d] opened %s\n", r.Process.Executable.Path, r.Process.AuditToken.Pid, r.Event.Open.File.Path)
 		case "exec":
-			fmt.Printf("%s [%d] launched %s\n", r.Process.Executable.Path, r.Process.ParentAuditToken.Pid, r.Event.Exec.Target.Executable.Path)
+			fmt.Printf("%s [%d] launched %s [%d]\n", r.Process.Executable.Path, r.Process.Ppid, r.Event.Exec.Target.Executable.Path, r.Process.AuditToken.Pid)
 		default:
 			fmt.Printf("%s: %+v", o.Kind, r)
 		}
